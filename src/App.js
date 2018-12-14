7949525b59d4aa0f170cfd1eeee39c63438a4237
Anh Le <anh.le@beteasy.com>
2018-12-14 19:17:39 +1100

    fix: Remove the css file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar'
-import './App.css';
 import CastList from './components/CastList';
 
 class App extends Component {
